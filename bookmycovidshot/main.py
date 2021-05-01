cbb45ef4b2dcc92686a1684ead2e64931e7c6367
Gaurav Lalchandani <glalchandanig@gmail.com>
2021-05-02 00:21:42 +0530

    Add index page with basic form
    Now, start connect this to backend. And obviously, need to
    improve the overall look and feel of the page.  But let's just
    make it functional first :smiley:

---
+++ bookmycovidshot/main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/alert')
+def alert():
+    return render_template('alert.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
