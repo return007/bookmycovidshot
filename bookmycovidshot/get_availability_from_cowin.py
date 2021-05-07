3dd233f2f265dfee3d2331ca61c7cde6cf7ee9dd
sachin-ks <sainisachin1210@gmail.com>
2021-05-07 22:13:20 +0530

    Increase timeout for cowin API

--- bookmycovidshot/get_availability_from_cowin.py
+++ bookmycovidshot/get_availability_from_cowin.py
@@ -26,7 +26,7 @@ def fetch_from_cowin(url):
                 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
                               '(KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
                 'Referer': 'https://www.cowin.gov.in/'
-            }, timeout=1
+            }, timeout=3
         )
     except requests.exceptions.RequestException as e:
         print(e)
